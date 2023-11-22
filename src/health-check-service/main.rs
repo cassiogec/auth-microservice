4ffbacf9e9c32911e018b1dde50c18cf1e1d4f8d
cassiogec <cassiogec@gmail.com>
2023-11-22 17:37:10 -0300

    stage-1/step-4 -> Implementing client cli

--- src/health-check-service/main.rs
+++ src/health-check-service/main.rs
@@ -3,10 +3,7 @@ use std::env;
 use authentication::auth_client::AuthClient;
 use authentication::{SignInRequest, SignOutRequest, SignUpRequest};
 use tokio::time::{sleep, Duration};
-use tonic::{Request, Response};
 use uuid::Uuid;
-
-use crate::authentication::{SignInResponse, SignOutResponse, SignUpResponse};
 
 pub mod authentication {
     tonic::include_proto!("authentication");
@@ -14,25 +11,21 @@ pub mod authentication {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // AUTH_SERVICE_HOST_NAME will be set to 'auth' when running the health check service in Docker
-    // ::0 is required for Docker to work: https://stackoverflow.com/questions/59179831/docker-app-server-ip-address-127-0-0-1-difference-of-0-0-0-0-ip
     let auth_hostname = env::var("AUTH_SERVICE_HOST_NAME").unwrap_or("[::0]".to_owned());
 
-    // Establish connection when auth service
     let mut client = AuthClient::connect(format!("http://{}:50051", auth_hostname)).await?;
 
     loop {
-        let username: String = Uuid::new_v4().to_string();
-        let password: String = Uuid::new_v4().to_string();
+        let username = Uuid::new_v4().to_string();
+        let password = Uuid::new_v4().to_string();
 
-        let request: Request<SignUpRequest> = Request::new(SignUpRequest {
+        let request = tonic::Request::new(SignUpRequest {
             username: username.clone(),
             password: password.clone(),
         });
 
-        let response: Response<SignUpResponse> = client.sign_up(request).await?;
+        let response = client.sign_up(request).await?;
 
-        // Log the response
         println!(
             "SIGN UP RESPONSE STATUS: {:?}",
             response.into_inner().status_code()
@@ -40,26 +33,26 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
         // ---------------------------------------------
 
-        let request: Request<SignInRequest> = Request::new(SignInRequest {
+        let request = tonic::Request::new(SignInRequest {
             username: username.clone(),
             password: password.clone(),
         });
 
-        let response: SignInResponse = client.sign_in(request).await?.into_inner();
+        let response = client.sign_in(request).await?.into_inner();
 
         println!("SIGN IN RESPONSE STATUS: {:?}", response.status_code());
 
         // ---------------------------------------------
 
-        let request: Request<SignOutRequest> = Request::new(SignOutRequest {
+        let request = tonic::Request::new(SignOutRequest {
             session_token: response.session_token,
         });
 
-        let response: Response<SignOutResponse> = client.sign_out(request).await?;
+        let response = client.sign_out(request).await?;
 
         println!(
             "SIGN OUT RESPONSE STATUS: {:?}",
-            response.into_inner().status_code
+            response.into_inner().status_code()
         );
 
         println!("--------------------------------------",);
